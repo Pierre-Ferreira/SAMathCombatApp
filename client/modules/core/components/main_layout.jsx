ba85cb9f1e10315655a43e25f30bbc389a940551
Pierre Ferreira <pierre@tektite.biz>
2016-11-25 18:08:15 +0200

    After ‘npm remove react-favicon --save’ to add a flavicon (Remember the name must be flavicon.ico). After ‘meteor add kadira:dochead’ to easily add/change <head>.

--- client/modules/core/components/main_layout.jsx
+++ client/modules/core/components/main_layout.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Favicon from 'react-favicon';
+
+const title = 'SAMathCombat'
+DocHead.setTitle(title)
+
+const metaInfo = { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+DocHead.addMeta(metaInfo)
+
+const fontInfo = { name: 'link', href: 'https://fonts.googleapis.com/css?family=Bangers:300,400,500|Bonbon|Bungee+Shade|Creepster|Faster+One|Finger+Paint|Frijole|Hanalei:300,400,500|Londrina+Shadow|Luckiest+Guy|Monofett|Nosifer|Press+Start+2P|Roboto:300,400,500|Shojumaru|Slackey', type: 'text/css' }
+DocHead.addLink(fontInfo)
 
 const Layout = ({content = () => null }) => (
   <div>
+    <Favicon url={['/src/favicon.ico']}/>
     <div>
       {content()}
     </div>
